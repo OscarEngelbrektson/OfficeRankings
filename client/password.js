7a0732a5bbc6fba61beaeab0109f69026752016d
oscar engelbrektson <engelbrektson.oscar@bcg.com>
2023-03-14 20:14:18 +0100

    update password

--- client/password.js
+++ client/password.js
@@ -1,4 +1,4 @@
-const PASSWORD = String.fromCharCode(100-3)+String.fromCharCode(3*30+7)+String.fromCharCode((1<<2<<2<<2)+33)
+const PASSWORD = String.fromCharCode(68 - 3) + String.fromCharCode(3 * 30 + 7) + String.fromCharCode((1 << 2 << 2 << 2) + 33)
 export let verifyPassword = () => {
     if (localStorage.password == PASSWORD) {
         return true
@@ -11,5 +11,5 @@ export let verifyPassword = () => {
             let tryAgain = confirm('Wrong password, please try again (see slide poster on 7th floor)')
             return tryAgain ? verifyPassword() : false
         }
-    } 
+    }
 }
